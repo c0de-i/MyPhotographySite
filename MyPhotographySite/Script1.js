708dbcf126e0e6fdb92e4154149197cedf723526
c0de-i <94860471+c0de-i@users.noreply.github.com>
2022-04-14 16:08:39 +0530

    Add files via upload
    changes by c0de_i

---
+++ MyPhotographySite/Script1.js
@@ -0,0 +1,22 @@
+/* JavaScript source code
+let cars = ["bmw","volvo","Honda"]
+document.getElementsById("container2").innerHTML = cars;
+
+
+let bmw = cars[0];
+document.getElementsById("container2").innerHTML = bmw;
+
+let len = cars.length
+document.getElementsById("container2").innerHTML = len;
+
+let last = cars.pop();
+document.getElementsById("container2").innerHTML = last;
+
+let bikes = ["yamha","sux","hero"];
+document.getElementsById("container2").innerHTML = bikes;
+
+let num1 = [2,3,1,4,5,6,7]
+num1.sort();
+document.getElementsById("container2").innerHTML= num1;
+*/
+
